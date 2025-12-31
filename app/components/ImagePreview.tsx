550a489c350c5e382776e07a0aee6a65da2047f7
gascheckking <warpaiactivity1@gmail.com>
2026-01-01 00:21:25 +0100

    Update ImagePreview.tsx

--- app/components/ImagePreview.tsx
+++ app/components/ImagePreview.tsx
@@ -2,7 +2,7 @@
 import { View, Image, StyleSheet, Dimensions } from 'react-native';
 
 const { width } = Dimensions.get('window');
-const IMAGE_SIZE = width / 3 - 20;
+const IMAGE_SIZE = width / 3 - 28;
 
 type Props = {
   uri: string;
@@ -18,14 +18,15 @@ export default function ImagePreview({ uri }: Props) {
 
 const styles = StyleSheet.create({
   container: {
-    margin: 5,
+    margin: 8,
     borderRadius: 12,
     overflow: 'hidden',
-    elevation: 3,
+    elevation: 4,
     shadowColor: '#000',
     shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
+    shadowOpacity: 0.15,
+    shadowRadius: 6,
+    backgroundColor: '#f0f0f0',
   },
   image: {
     width: IMAGE_SIZE,
