51f67c8dddc6b56b9cbc35e3f2d00cee2a1ab633
gascheckking <warpaiactivity1@gmail.com>
2026-01-11 19:45:19 +0100

    Update BuyCard.tsx

--- components/BuyCard.tsx
+++ components/BuyCard.tsx
@@ -4,7 +4,7 @@
 // ─────────────────────────────────────────────
 
 import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
-import { theme } from '../theme';
+import { theme } from '../app/theme'
 import MatchScore from './MatchScore';
 import LiquidBadge from './LiquidBadge';
 
