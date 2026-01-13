94acb7853c9d6bf3b94bdb6a28c0c932f7afa488
gascheckking <warpaiactivity1@gmail.com>
2026-01-13 18:49:58 +0100

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
+// app/_layout.tsx
 import { Stack } from 'expo-router';
 
 export default function RootLayout() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      {/* Tabs (Home, Sell, etc) */}
       <Stack.Screen name="(tabs)" />
+
+      {/* Upload flow (modal/stack) */}
       <Stack.Screen name="upload" />
     </Stack>
   );
