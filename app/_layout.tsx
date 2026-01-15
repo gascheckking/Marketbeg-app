80f52b04826a164663e132b63fbce8ea16060a89
gascheckking <warpaiactivity1@gmail.com>
2026-01-15 09:11:48 +0100

    Update _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,12 @@
 // app/_layout.tsx
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
   return (
     <>
       <StatusBar style="light" />
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" />
-        <Stack.Screen name="upload" />
-        <Stack.Screen name="trade" />
-      </Stack>
+      <Slot />
     </>
   );
 }
