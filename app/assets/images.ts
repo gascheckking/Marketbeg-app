b21a3b118dc8f50eed08d7bf5fde6bb39536e36c
gascheckking <warpaiactivity1@gmail.com>
2026-01-16 12:45:58 +0100

    Create images.ts

---
+++ app/assets/images.ts
@@ -0,0 +1,8 @@
+// app/assets/images.ts
+
+export const images = {
+  arbetsklader: require('./arbetsklader.png'),
+  skor: require('./skor.PNG'),
+  markesvaskor: require('./markesvaskor.PNG'),
+  vintagefynd: require('./vintagefynd.PNG'),
+};
